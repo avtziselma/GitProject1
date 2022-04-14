38a4cea9a818ab8a01857f5a05949adde44908fe
avtziselma <98188272+avtziselma@users.noreply.github.com>
2022-04-14 19:50:16 +0100

    work 2 is done

--- src/work1/HelloWorld.java
+++ src/work1/HelloWorld.java
@@ -2,6 +2,10 @@ package work1;
 
 public class HelloWorld {
     // Doing some work here (you can type anything you want)
+
+    // doing awesome work 2!
+
+
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
